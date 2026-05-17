0c07ddaac2de7e282660b85cd1c11f2e9f7298ec
Cady C <chiucady107@gmail.com>
2026-05-16 17:30:01 -0700

    Update main.jsx, fixed imports

--- heart-to-mom/src/main.jsx
+++ heart-to-mom/src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import './index.css'
+import Auth from './pages/Auth.jsx'
+import Home from './pages/Home.jsx'
+import Onboarding from './pages/Onboarding.jsx'
+import TrackHealth from './pages/TrackHealth.jsx'
 import SyncWearable from './pages/SyncWearable.jsx'
 import Prepare from './pages/Prepare.jsx'
 import { AuthProvider } from './lib/AuthContext.jsx'
-import ProtectedRoute from './lib/ProtectedRoute.jsx'
-import Chatbot from './components/Chatbot'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
