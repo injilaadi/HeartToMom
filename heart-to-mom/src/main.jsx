3d81959955936cb98c3e4473b506fef6a09aa39f
Cady C <chiucady107@gmail.com>
2026-05-16 17:26:27 -0700

    Update main.jsx
    Added ProtectedRoute back + Chatbot import
    - requires user to log in/create account first before going into pages

--- heart-to-mom/src/main.jsx
+++ heart-to-mom/src/main.jsx
@@ -1,12 +1,6 @@
-// Removed ProtectedRoute for ease of access when testing
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import './index.css'
-import Auth from './pages/Auth.jsx'
-import Home from './pages/Home.jsx'
-import Onboarding from './pages/Onboarding.jsx'
-import TrackHealth from './pages/TrackHealth.jsx'
 import SyncWearable from './pages/SyncWearable.jsx'
 import Prepare from './pages/Prepare.jsx'
 import { AuthProvider } from './lib/AuthContext.jsx'
@@ -18,37 +12,46 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Auth />} />
           <Route
             path="/onboarding"
             element={
-              <Onboarding />
+              <ProtectedRoute>
+                <Onboarding />
+              </ProtectedRoute>
             }
           />
-
           <Route
             path="/home"
             element={
-              <Home />
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
             }
           />
           <Route
             path="/track-health"
             element={
-              <TrackHealth />
+              <ProtectedRoute>
+                <TrackHealth />
+              </ProtectedRoute>
             }
           />
           <Route
             path="/sync-wearable"
             element={
-              <SyncWearable />
+              <ProtectedRoute>
+                <SyncWearable />
+              </ProtectedRoute>
             }
           />
           <Route
             path="/prepare"
             element={
-              <Prepare />
+              <ProtectedRoute>
+                <Prepare />
+              </ProtectedRoute>
             }
           />
         </Routes>
@@ -56,4 +59,3 @@ createRoot(document.getElementById('root')).render(
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
-)
